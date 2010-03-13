af78c3ac9eafb063ed247047feaee8af1de2bf0d
Joao P Dubas <joao_dubas@yahoo.com.br>
2010-03-13 00:38:51 -0300

    Solved problem with register of students on the array.

--- media/javascript/logica_aula_2.js
+++ media/javascript/logica_aula_2.js
@@ -49,6 +49,12 @@ function validateInteiroPositivo(valor) {
     }
     return numero_validacao;
 }
+/**
+ * Verifica se o elemento contem a classe especifica
+ * @param target {HTMLElement}: elemento a ser avaliado
+ * @param nome_class {String}: a classe sendo procurada
+ * @return {Boolean} indicando se class foi ou nao encontrada
+ */
 function hasClass(target, nome_classe) {
     var target_class = target.className,
         regexp_class = new RegExp('(^| )' + nome_classe + '( |$)');
@@ -58,8 +64,13 @@ function hasClass(target, nome_classe) {
     }
     return bl_has_class;
 }
+/**
+ * Adiciona uma classe ao elemento, caso ja nao exista
+ * @param target {HTMLElement}: elemento a ser avaliado
+ * @param nome_class {String}: a classe sendo adicionada
+ */
 function addClass(target, class_name) {
-    if (hasClass(target, class_name)) {
+    if (!hasClass(target, class_name)) {
         if (target.className == '') {
             target.className = class_name;
         } else {
@@ -67,18 +78,43 @@ function addClass(target, class_name) {
         }
     }
 }
+/**
+ * Remove uma classe do elemento, caso este possua
+ * @param target {HTMLElement}: elemento a ser avaliado
+ * @param nome_class {String}: a classe sendo removida
+ */
 function removeClass(target, class_name) {
-    var regexp_class = new RegExp('(^| )' + className + '( |$)');
+    var regexp_class = new RegExp('(^| )' + class_name + '( |$)');
     if (hasClass(target, class_name)) {
-        target.className = target.className.replace(pattern, '');
+        target.className = target.className.replace(regexp_class, '');
     }
 }
+/**
+ * Altera uma classe do elemento para outra
+ * @param target {HTMLElement}: elemento a ser avaliado
+ * @param class_to_remove {String}: classe a ser removida
+ * @param class_to_add {String}: classe a ser adicionada
+ */
 function changeClass(target, class_to_remove, class_to_add) {
     removeClass(target, class_to_remove);
     addClass(target, class_to_add);
 }
 
 //--- CONTROLES ---------------------------------------------------
+function formatLog(dados) {
+    var log = {};
+    if (dados.length === 3) {
+        log.origem = 'aluno';
+        log.nome = dados[0];
+        log.idade = dados[1];
+        log.numero_aluno = dados[2];
+    } else {
+        log.origem = 'media';
+        log.idade = dados[0];
+        log.numero_aluno = dados[1];
+    }
+    return log;
+}
 /**
  * Controla a acao dos botoes do formulario
  * @param {Object} e: o objeto evento
@@ -91,7 +127,8 @@ function controlCadastro(e) {
         quadro_formulario, quadro_resposta,
         btn_value = '', ipt_nome = '', ipt_idade = '',
         vl_idade = {}, nr_idade = 0,
-        soma_idade = 0, total_aluno = 0, media_idade = 0;
+        soma_idade = 0, total_aluno = 0, media_idade = 0,
+        log = {};
     if (btn_target.nodeName.toLowerCase() === 'button') {
         quadro_formulario = doc.getElementById('formulario');
         quadro_resposta = doc.getElementById('resposta');
@@ -104,6 +141,8 @@ function controlCadastro(e) {
             if (vl_idade.valido) {
                 nr_idade = vl_idade.valor;
                 wdw._arr_idade.push(nr_idade);
+                log = formatLog([ipt_nome.value, ipt_idade.value, wdw._arr_idade.length]);
+                setLog(log);
             } else {
                 alert(vl_idade.mensagem);
                 ipt_idade.value = '';
@@ -119,6 +158,8 @@ function controlCadastro(e) {
                     soma_idade += wdw._arr_idade[i];
                 }
                 media_idade = soma_idade / total_aluno;
+                log = formatLog([media_idade, total_aluno]);
+                setLog(log);
             }
         } else if (btn_value === 'inserir') {
             wdw._arr_idade = [];
@@ -133,7 +174,23 @@ function controlCadastro(e) {
  * Apresenta um log em tela da acao realizada
  */
 function setLog(obj_dado) {
-    var doc = document;
+    var doc = document,
+        log = doc.getElementById('log'),
+        log_child = log.getElementsByTagName('ul').length,
+        log_list,
+        list_node, text_node = '';
+    if (log_child === 0) {
+        log.appendChild(doc.createElement('ul'));
+    }
+    log_list = log.getElementsByTagName('ul')[0];
+    list_node = doc.createElement('li');
+    if (obj_dado.origem === 'aluno') {
+        text_node = doc.createTextNode(obj_dado.nome + ' (' + obj_dado.idade + '): ' + obj_dado.numero_aluno);
+    } else {
+        text_node = doc.createTextNode('Media: ' + obj_dado.media + '. Em turma de ' + obj_dado.numero_aluno + ' alunos');
+    }
+    list_node.appendChild(text_node);
+    log_list.appendChild(list_node);
 }
 
 /**
@@ -143,7 +200,15 @@ function init() {
     document.getElementById('formulario').onclick = function (e) {
         controlCadastro(e);
     };
-    document.getElementById('frm_aluno').onsubmit = function () {
+    /*
+    document.getElementById('frm_aluno').onsubmit = function (e) {
+        e = getEvent(e);
+        if (typeof event.preventDefault !== 'undefined') {
+            e.preventDefault();
+        } else {
+            e.returnValue = false;
+        }
         return false;
     };
+    */
 }
