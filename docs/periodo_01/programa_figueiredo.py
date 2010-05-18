e96d83a83b06c3a360827f2e42fa9959a8a5c837
Joao P Dubas <joao_dubas@yahoo.com.br>
2010-05-18 00:20:05 -0300

    Adicionado possibilidade de ver novo resultado
    Adicionada possibilidade para adicionar novo resultado alem de, iniciado
    trabalho para conversao das datas e horas para timestamp.

--- docs/periodo_01/programa_figueiredo.py
+++ docs/periodo_01/programa_figueiredo.py
@@ -74,7 +74,7 @@ def __validar(valor, *args):
     else:
         if args[0] <= valor <= args[1]:
             flag = True
-    
+
     return flag
 
 def __validar_ano(ano, validar):
@@ -83,7 +83,7 @@ def __validar_ano(ano, validar):
     '''
     if __validar(ano, 0):
         validar.append('ano_entrada')
-    
+
     return validar
 
 def __validar_mes(mes, validar):
@@ -93,7 +93,7 @@ def __validar_mes(mes, validar):
     if 'ano_entrada' in validar:
         if __validar(mes, 1, 12):
             validar.append('mes_entrada')
-    
+
     return validar
 
 def __validar_dia(dia, dia_limite, validar):
@@ -102,7 +102,7 @@ def __validar_dia(dia, dia_limite, validar):
     '''
     if __validar(dia, 1, dia_limite):
         validar.append('dia_entrada')
-    
+
     return validar
 
 def __definir_dia_limite(mes, ano, validar):
@@ -111,7 +111,7 @@ def __definir_dia_limite(mes, ano, validar):
     '''
     mes_trinta_dias = [4, 6, 9, 11]
     dia_limite = 0
-    
+
     if 'mes_entrada' in validar:
         if mes == 2:
             if (ano % 4) == 0:
@@ -122,7 +122,7 @@ def __definir_dia_limite(mes, ano, validar):
             dia_limite = 30
         else:
             dia_limite = 31
-    
+
     return dia_limite
 
 def __validar_hora(hora, validar):
@@ -131,7 +131,7 @@ def __validar_hora(hora, validar):
     '''
     if __validar(hora, 0, 23):
         validar.append('hora_entrada')
-    
+
     return validar
 
 def __validar_minuto(minuto, validar):
@@ -140,7 +140,7 @@ def __validar_minuto(minuto, validar):
     '''
     if __validar(minuto, 0, 59):
         validar.append('minuto_entrada')
-    
+
     return validar
 
 def __validar_temperatura(temperatura, validar):
@@ -149,7 +149,7 @@ def __validar_temperatura(temperatura, validar):
     '''
     if __validar(temperatura, 1):
         validar.append('temperatura_entrada')
-    
+
     return validar
 
 def __obter_tempo_saida(dic_temperatura, temperatura):
@@ -161,7 +161,7 @@ def __obter_tempo_saida(dic_temperatura, temperatura):
         temperatura = 120
     else:
         temperatura = 121
-    
+
     return dic_temperatura.get(temperatura)
 
 def __calcular_timestamp_retirada(**kwargs):
@@ -172,6 +172,7 @@ def __calcular_timestamp_retirada(**kwargs):
     hora_saida = kwargs.get('hora')
     minuto_saida = kwargs.get('minuto')
     minuto_preparo = kwargs.get('minuto_preparo')
+    validar = kwargs.get('validar')
 
     while minuto_preparo >= 0:
         if minuto_saida > 59:
@@ -181,19 +182,24 @@ def __calcular_timestamp_retirada(**kwargs):
         if hora_saida > 23:
             hora_saida = 0
             dia_saida += 1
-        
+
         if dia_saida > dia_limite:
             dia_saida = 1
             mes_saida += 1
-        
+            if mes_saida <= 12
+                dia_limite = __definir_dia_limite(mes_saida, ano_saida, validar)
+
         if mes_saida > 12:
             mes_saida = 1
             ano_saida += 1
-        
+            dia_limite = __definir_dia_limite(mes_saida, ano_saida, validar)
+
         minuto_preparo -= 1
         if minuto_preparo >= 0:
             minuto_saida += 1
-    
+
+    timestamp_saida = datetime.datetime(ano_saida, mes_saida, dia_saida,    \
+            hora_saida, minuto_saida)
     return {
             'dia_entrada': kwargs.get('dia')            \
             , 'mes_entrada': kwargs.get('mes')          \
@@ -221,7 +227,7 @@ def __definir_tela_retorno(dc_valores):
 
     retorno = '\n'.join(tela)
     retorno = retorno % dc_valores
-    
+
     return retorno
 
 def obter_data_hora_retirada(**kwargs):
@@ -230,58 +236,63 @@ def obter_data_hora_retirada(**kwargs):
     produto
     '''
     timestamp_hoje = datetime.today()
-    
+    timestamp_entrada
+
     validar = []
     detalhes_fornada = {}
 
     codigo_produto = kwargs.get('codigo', 'BR1')
     detalhes_produto = PRODUTO.get(codigo_produto)
     descricao_produto = detalhes_produto.get('descricao')
-    temperatura_produto = detalhes_produto.get('temperatura')   
-    
+    temperatura_produto = detalhes_produto.get('temperatura')
+
     temperatura_entrada = kwargs.get('temperatura', 80)
-    
+
     dia_entrada = kwargs.get('dia', timestamp_hoje.day)
     mes_entrada = kwargs.get('mes', timestamp_hoje.month)
     ano_entrada = kwargs.get('ano', timestamp_hoje.year)
-    
+
     hora_entrada = kwargs.get('hora', timestamp_hoje.hour)
     minuto_entrada = kwargs.get('minuto', timestamp_hoje.minute)
-    
+
     minuto_saida = 0
-    
+
     dia_limite = 0
 
     #validar ano
     validar = __validar_ano(ano_entrada, validar)
-    
+
     #validar mês
     validar = __validar_mes(mes_entrada, validar)
 
     #calcular dia limite
     dia_limite = __definir_dia_limite(mes_entrada, ano_entrada, validar)
-    
+
     #validar dia
     validar = __validar_dia(dia_entrada, dia_limite, validar)
-    
+
     #validar hora
     validar = __validar_hora(hora_entrada, validar)
-    
+
     #validar minuto
     validar = __validar_minuto(minuto_entrada, validar)
-    
+
     #validar temperatura
     validar = __validar_temperatura(temperatura_entrada, validar)
 
+    timestamp_entrada = datetime.datetime(ano_entrada, mes_entrada, dia_entrada,\
+            hora_entrada, minuto_entrada)
+
     minuto_saida = __obter_tempo_saida(temperatura_produto, temperatura_entrada)
 
-    detalhes_fornada = __calcular_timestamp_retirada(dia_limite = dia_limite
-            , dia = dia_entrada
-            , mes = mes_entrada
-            , ano = ano_entrada
-            , hora = hora_entrada
-            , minuto = minuto_entrada
-            , minuto_preparo = minuto_saida)
+    detalhes_fornada = __calcular_timestamp_retirada(dia_limite = dia_limite    \
+            , dia = dia_entrada             \
+            , mes = mes_entrada             \
+            , ano = ano_entrada             \
+            , hora = hora_entrada           \
+            , minuto = minuto_entrada       \
+            , minuto_preparo = minuto_saida \
+            , validar = validar)
 
     detalhes_fornada.setdefault('codigo_produto', codigo_produto)
     detalhes_fornada.setdefault('descricao_produto', descricao_produto)
@@ -294,51 +305,55 @@ if __name__ == '__main__':
     msg_numero_invalido = u'Digite um número para %s'
 
     codigo_produto = ''
+    novo_produto = 'sim'
     dia = mes = ano = hora = minuto = temperatura = dia_limite = -1
 
-    while not PRODUTO.has_key(codigo_produto):
-        codigo_produto = str(input(u'Codigo do produto: '))
+    while novo_produto == 'sim':
+        while not PRODUTO.has_key(codigo_produto):
+            codigo_produto = str(input(u'Codigo do produto: '))
 
-    while 'temperatura_entrada' not in validar:
-        try:
-            temperatura = int(input(u'Temperatura de preparo: '))
-            validar = __validar_temperatura(temperatura, validar)
-        except ValueError:
-            print msg_numero_invalido % 'a temperatura'
-            continue
+        while 'temperatura_entrada' not in validar:
+            try:
+                temperatura = int(input(u'Temperatura de preparo: '))
+                validar = __validar_temperatura(temperatura, validar)
+            except ValueError:
+                print msg_numero_invalido % 'a temperatura'
+                continue
 
-    while 'ano_entrada' not in validar          \
-            and 'mes_entrada' not in validar    \
-            and 'dia_entrada' not in validar:
-        try:
-            dia = int(input('Dia da entrada: '))
-            mes = int(input(u'Mes da entrada: '))
-            ano = int(input('Ano da entrada: '))
-            
-            validar = __validar_ano(ano, validar)
-            validar = __validar_mes(mes, validar)
-            dia_limite = __definir_dia_limite(mes, ano, validar)
-            validar = __validar_dia(dia, dia_limite, validar)
-        except ValueError:
-            print msg_numero_invalido % 'a data'
-            continue
+        while 'ano_entrada' not in validar          \
+                and 'mes_entrada' not in validar    \
+                and 'dia_entrada' not in validar:
+            try:
+                dia = int(input('Dia da entrada: '))
+                mes = int(input(u'Mes da entrada: '))
+                ano = int(input('Ano da entrada: '))
 
-    while 'hora_entrada' not in validar \
-            and 'minuto_entrada' not in validar:
-        try:
-            hora = int(input(u'Hora de entrada: '))
-            minuto = int(input(u'Minuto de entrada: '))
+                validar = __validar_ano(ano, validar)
+                validar = __validar_mes(mes, validar)
+                dia_limite = __definir_dia_limite(mes, ano, validar)
+                validar = __validar_dia(dia, dia_limite, validar)
+            except ValueError:
+                print msg_numero_invalido % 'a data'
+                continue
 
-            validar = __validar_hora(hora, validar)
-            validar = __validar_minuto(minuto, validar)
-        except ValueError:
-            print msg_numero_invalido % u'o horário'
-            continue
+        while 'hora_entrada' not in validar \
+                and 'minuto_entrada' not in validar:
+            try:
+                hora = int(input(u'Hora de entrada: '))
+                minuto = int(input(u'Minuto de entrada: '))
 
-    print obter_data_hora_retirada(codigo = codigo_produto  \
-            , temperatura = temperatura \
-            , dia = dia                 \
-            , mes = mes                 \
-            , ano = ano                 \
-            , hora = hora               \
-            , minuto = minuto)
+                validar = __validar_hora(hora, validar)
+                validar = __validar_minuto(minuto, validar)
+            except ValueError:
+                print msg_numero_invalido % u'o horário'
+                continue
+
+        print obter_data_hora_retirada(codigo = codigo_produto  \
+                , temperatura = temperatura \
+                , dia = dia                 \
+                , mes = mes                 \
+                , ano = ano                 \
+                , hora = hora               \
+                , minuto = minuto)
+
+        novo_produto = str(input('Novo produto (sim ou nao) '))
