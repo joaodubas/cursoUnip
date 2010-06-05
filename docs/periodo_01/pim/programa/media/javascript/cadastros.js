f269b83300e5c750a9ddc9321f500e4108ab51b9
Joao Paulo Dubas <joao_dubas@yahoo.com.br>
2010-06-05 20:26:11 -0300

    Implementacao dos metodos para controle da pagina

--- docs/periodo_01/pim/programa/media/javascript/cadastros.js
+++ docs/periodo_01/pim/programa/media/javascript/cadastros.js
@@ -16,21 +16,65 @@ var CADASTRO =
     {
         formulario: 'avaliacao_cadastro'
         , filtro: 'avaliacao_filtro'
-        , tabela
+        , tabela: 'avaliacao_registro'
     }
 };
 var RegistroTable = new Class({
     Implements: [Options]
     , options: {
-        tabela_registro: $('registro')
-        , formulario: $('cadastro')
-        , registros: $.jStorage.get('registro')
-        , tipo: CADASTRO_TURMA
+        tipo: CADASTRO['cadastro_turma']
+        , registros: null
     }
+    , tabela: null
+    , filtro: null
+    , formulario: null
+    , modal: null
+    , adicionar: null
     , initialize: function (options) {
         this.setOptions(options);
+        this.tabela = $(this.options.tipo['tabela']);
+        this.filtro = $(this.options.tipo['filtro']);
+        this.formulario = $(this.options.tipo['formulario']);
+        this.modal = this.formulario.getParent();
+        this.modal.addClass('esconder');
+        this.adicionar = $$('.btn_add_registro');
+        this.adicionarControle();
     }
     , adicionarControle: function () {
+        var modal = this.modal
+            , modalSize = null
+            , viewPort = null
+            , topPos = 0
+            ;
+        this.formulario.addEvents({
+            'submit': function (event) {
+                event.preventDefault();
+                modal.removeClass('exibir');
+                modal.addClass('esconder');
+                return false;
+            }
+            , 'reset': function (event) {
+                modal.removeClass('exibir');
+                modal.addClass('esconder');
+            }
+        });
+        this.tabela.addEvent('click:relay(a)', function (event, element) {
+            event.preventDefault();
+            alert('Nao implementado!');
+            return false;
+        });
+        this.adicionar.addEvent('click', function (event) {
+            event.preventDefault();
+            if (!modal.hasClass('exibir')) {
+                modal.removeClass('esconder');
+                modal.addClass('exibir');
+                modalSize = modal.getSize();
+                viewPort = window.getSize();
+                topPos = (viewPort.y - modalSize.y) / 2;
+                modal.setStyle('top', topPos);
+            }
+            return false;
+        });
         return false;
     }
     , adicionarRegistro: function () {
@@ -49,26 +93,22 @@ var RegistroTable = new Class({
  */
 function getArquivo() {
     var endereco = window.location
-    , path = []
     , arquivo = ''
     ;
 
-    path = endereco.pathname.split('/');
-    arquivo = path[path.length - 1];
+    arquivo = endereco.pathname.split('/').getLast();
     arquivo = arquivo.substring(0, arquivo.indexOf('.'));
 
     return arquivo;
 }
 
 window.addEvent('domready', function () {
-    var frm_flt_turma = $('filtro_turma')       //form de filtro de turma
-        , frm_cad_turma = $('turma_cadastro')   //form de cadastro de turma
-        , tbl_reg_turma = $('turma_registro')   //tabela de registro de turma
-        , cadastro_classe = $.jStorage.get('classes')   //registro de classes
+    var nome_cadastro = getArquivo()                        //nome pagina
+        , cadastro_classe = $.jStorage.get('classes', null) //registro de classes
+        , tipo_cadastro = CADASTRO[nome_cadastro]           //tipo de cadastro
+        , controleForm = new RegistroTable({
+            tipo: tipo_cadastro
+            , registros: cadastro_classe
+        })
     ;
-
-    frm_cad_turma.addEvent('submit', function () {
-        alert('tentou salvar mas nao pode!!!');
-        return false;
-    });
 });
