4552074c42942c646154a7aa14b2596db672678b
Joao P Dubas <joao_dubas@yahoo.com.br>
2010-06-09 18:59:56 -0300

    Mixin para dados de testes

---
+++ docs/periodo_01/pim/programa/media/javascript/dados_teste.js
@@ -0,0 +1,24 @@
+var teste = [
+    {
+        nr_id_turma: 0
+        , nm_turma: '6ª série'
+        , cd_periodo: 1
+        , alunos: [
+            {
+                nr_id_aluno: 0
+                , nm_aluno: 'João Paulo Dubas'
+                , dt_nascimento: '15/12/1995'
+                , avaliacoes: [
+                    {
+                        nr_id_avaliacao: 0
+                        , data_avaliacao: '15/06/2009'
+                        , nr_massa: 49
+                        , nr_estatura: 165
+                        , nr_cintura: 67
+                        , nr_quadril: 78
+                    }
+                ]
+            }
+        ]
+    }
+];
